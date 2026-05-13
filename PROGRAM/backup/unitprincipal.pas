905d9f9aeaa2f725c84fd6aceb054a92d625fcaa
Cristianoltfs <cristianoltfs@gmail.com>
2026-05-13 09:47:22 -0300

    resitência à tração e compressão

--- PROGRAM/backup/unitprincipal.pas
+++ PROGRAM/backup/unitprincipal.pas
@@ -12,18 +12,26 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
-    Button1: TButton;
+    btnSair: TButton;
+    btnCalcular: TButton;
+    edtRC: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    lblRT: TLabel;
     MainMenu1: TMainMenu;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
     MenuItem4: TMenuItem;
-    procedure Button1Click(Sender: TObject);
+    procedure btnCalcularClick(Sender: TObject);
+    procedure btnSairClick(Sender: TObject);
     procedure MenuItem2Click(Sender: TObject);
   private
 
   public
-
+    rt : real; // resistência à tração da rocha em MPa
+    rtStr : string; // para exibir como string no formulário
+    rc : integer; // resistência à compressão da rocha me MPa
   end;
 
 var
@@ -35,9 +43,17 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.Button1Click(Sender: TObject);
+procedure TForm1.btnSairClick(Sender: TObject);
 begin
   Application.Terminate;
+end;
+
+procedure TForm1.btnCalcularClick(Sender: TObject);
+begin
+  rc := StrToInt(edtRC.Text);
+  rt := rc / 10;
+  str(rt:4:0, rtStr);
+  lblRT.Caption := rtStr + 'MPa';
 end;
 
 procedure TForm1.MenuItem2Click(Sender: TObject);
